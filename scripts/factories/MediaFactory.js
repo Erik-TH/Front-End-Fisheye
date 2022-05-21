43a51f41da0589d8a8a8883e12347befa3829fe5
Erik <93394265+Erik-TH@users.noreply.github.com>
2022-05-21 11:33:50 +0200

    Added breakpoints

---
+++ scripts/factories/MediaFactory.js
@@ -0,0 +1,10 @@
+class MediaFactory {
+	constructor(mediaData, photographerFolder) {
+		const type = ('image' in mediaData) ? 'image' : 'video';
+		if (type === 'image') {
+			return new ImageMedia(mediaData, photographerFolder);
+		} else if (type === 'video') {
+			return new VideoMedia(mediaData, photographerFolder);
+		}
+	}
+}
